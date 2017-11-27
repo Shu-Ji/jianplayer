7ef645d0a94c70816df4edbdc1599c5cf1263b14
shuji <bh2@qq.com>
2017-11-28 07:47:13 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script lang="coffee">
+    export default
+        name: 'jianplayer'
+</script>
+
+<style>
+  /* CSS */
+</style>
---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="wrapper">
+    <main>
+      <div class="left-side">
+        <span class="title">
+          Welcome to your new project!
+        </span>
+        <system-information></system-information>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script lang="coffee">
+    import SystemInformation from './LandingPage/SystemInformation'
+
+    export default
+        default:
+            name: 'landing-page'
+            components: { SystemInformation }
+            methods:
+                open: (link) ->
+                    @$electron.shell.openExternal(link)
+</script>
+
+<style lang="scss">
+</style>
